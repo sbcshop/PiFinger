f1c1a72287085be9a07f52dd343cde302470fe5f
SB Components <gajender.singh@sb-components.co.uk>
2021-03-09 12:33:29 +0530

    Update remove_all_fp.py

--- Windows/remove_all_fp.py
+++ Windows/remove_all_fp.py
@@ -8,7 +8,7 @@ COM_PORT = "COM7"  # /dev/ttyS0"
 fp = FingerprintSensor()
 #  Initialise the sensor serial with the COM port and fixed baud rate of
 #  115200, set "use_thread" argument as false
-fp.connect_sensor(port=COM_PORT, baud_rate=115200, use_thread=False)
+fp.connect_sensor(port=COM_PORT, baud_rate=9600, use_thread=False)
 
 #  use remove_all_fingerprints function to remove all the fingerprints
 #  stored in the fingerprint sensor
